e6c3912670e757b141f36b8d97c5c3570dc2978c
Mukul Rajput <mr56323885@gmail.com>
2024-03-14 19:53:15 +0530

    first commit

--- src/app/api/users/logout/route.js
+++ src/app/api/users/logout/route.js
@@ -8,7 +8,7 @@ export async function GET(){
             message : "Logout Successfull",
             success: true
         })
-        response.cookies.set("token",'',{httpOnly: true , expires: new Date(0),path: '/'})
+        response.cookies.set("token",'',{httpOnly: true , expires: new Date(0),path: '/dashboard'})
         return response
     } catch (error) {
         return NextResponse.json({
