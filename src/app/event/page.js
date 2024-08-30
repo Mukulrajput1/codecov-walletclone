22176af21fcb16c14d849dddf742f232406acba8
Mukul <mr56323885@gmail.com>
2024-08-30 21:11:08 +0530

    opengraph card

--- src/app/event/page.js
+++ src/app/event/page.js
@@ -14,25 +14,25 @@ export async function generateMetadata({ searchParams }) {
       keywords: ["baby show", "events", "2024"],
       authors: [{ name: "Event Organizer" }],
       robots: "index, follow",
-      // openGraph: {
-      //   title,
-      //   description,
-      //   url: "https://perks.hireacoder.in",
-      //   images: [
-      //     {
-      //       url: imageUrl,
-      //       width: 800,
-      //       height: 600,
-      //       alt: title,
-      //     },
-      //   ],
-      // },
-      twitter: {
-        card: "summary_large_image",
+      openGraph: {
         title,
         description,
-        images: [imageUrl],
+        url: "https://perks.hireacoder.in",
+        images: [
+          {
+            url: imageUrl,
+            width: 800,
+            height: 600,
+            alt: title,
+          },
+        ],
       },
+      // twitter: {
+      //   card: "summary_large_image",
+      //   title,
+      //   description,
+      //   images: [imageUrl],
+      // },
     };
 }
 
